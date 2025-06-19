3af707bf9db551c9f6f5953ec74efeec265cc8de
corsarto <clerc.loic@gmail.com>
2025-06-19 16:55:43 +0200

    make error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import '../Error/error.scss'
 
 function Error () {
     return (
-        <div>
+        <div className='container-error'>
         <h1>404</h1>
-        <h2>Oups! La page que vous demandez n'existe pas.</h2>
-        <Link to="/">Retourner sur la page d'accueil</Link>
+        <h2>Oups! La page que <span>vous demandez n'existe pas.</span></h2>
+        <Link to="/" className='link-error'>Retourner sur la page d'accueil</Link>
         </div>
     )
 }
